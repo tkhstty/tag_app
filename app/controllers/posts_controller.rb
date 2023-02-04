8d9da418c7ce8a0d7a9ed30c79931898399ee079
tkhstty <tkhstty@gmail.com>
2023-02-04 12:00:09 +0900

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post_form = PostForm.new
+  end
+
+  def create
+    @post_form = PostForm.new(post_form_params)
+    if @post_form.valid?
+      @post_form.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    post_attributes = @post.attributes
+    @post_form = PostForm.new(post_attributes)
+    @post_form.tag_name = @post.tags&.first&.tag_name
+  end
+
+  def update
+    @post_form = PostForm.new(post_form_params)
+    @post_form.image ||= @post.image.blob
+
+    if @post_form.valid?
+      @post_form.update(post_form_params, @post)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def search
+    return nil if params[:keyword] == ""
+    tag = Tag.where(['tag_name LIKE ?', "%#{params[:keyword]}%"])
+    render json:{ keyword: tag }
+  end
+
+  private
+  def post_form_params
+    params.require(:post_form).permit(:text, :image, :tag_name)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  has_one_attached :image
+  has_many :post_tag_relations
+  has_many :tags, through: :post_tag_relations
+  validates :text, presence: true
+  validates :image, presence: true
+end
